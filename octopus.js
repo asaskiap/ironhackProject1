0d7852fd73d522628b6be49d645e783d2d2be698
asaskiap <alannapfeiffer@gmail.com>
2020-12-01 21:09:07 +0100

    changed start screen a little

--- octopus.js
+++ octopus.js
@@ -1,8 +1,11 @@
+const lastBossImg = new Image();
+lastBossImg.src = './images/lastBoss.png';
+
 class Octopus extends Item {
     constructor(x, y, dx, dy) {
         super(x, y, dx, dy);
         this.color = 'coral';
-        this.width = 100;
+        this.width = 300;
         this.height = 300;
         this.life = 100;
     }
@@ -23,7 +26,8 @@ class Octopus extends Item {
         this.y += this.dy;
     }
     draw() {
-        game.ctx.fillStyle = this.color;
-        game.ctx.fillRect(this.x, this.y, this.width, this.height);
+        // game.ctx.fillStyle = this.color;
+        // game.ctx.fillRect(this.x, this.y, this.width, this.height);
+        game.ctx.drawImage(lastBossImg, this.x, this.y, this.width, this.height);
     }
 }
